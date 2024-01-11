0956572dd469bf2f1513c4c9155c8683d0ce394b
EdwinZarateP <emzp1994@gmail.com>
2024-01-10 22:28:24 -0500

    modif3

--- src/componentes/Navbar.tsx
+++ src/componentes/Navbar.tsx
@@ -1,17 +1,48 @@
 import logo from '../imagenes/logo12.png';
 import '../estilos/Navbar.css'
+import { BiWorld } from "react-icons/bi";
+import { FiMenu, FiSearch } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
 
 const NavBar = () => {
     return (
-        <div>
+        <div className='barNav'>
             {/* Parte Izquierda */}
-            <div className="contenedorNavbar">
+            <div className="contenedorizquierdo">
                 <img src={logo} className="logo"/>
             </div>
 
             {/* Parte media */}
+            <div className='contenedormedio'>
+                
+                <input
+                className='inputmedio'
+                type='search'
+                placeholder=''
+                />
+
+                <div className='contenedorMedioBotones'>
+                    <button>Lugar</button>
+                    <button>Tiempo</button>
+                    <button>Grupo talla</button>
+                </div>
+                
+                <div className='lupaBusqueda'>
+                <FiSearch />
+                </div>
+
+            </div>
+
             {/* Parte derecha */}
-            <h3>Glamping Colombia</h3>
+            <div className='contenedorderecho'>
+                <p>Alquila un glamping</p>
+                <BiWorld />
+
+                <div>
+                    <FiMenu />
+                    <AiOutlineUser />
+                </div>
+            </div>
         </div>
     );
 };
