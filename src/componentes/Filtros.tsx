2c28f45586751ea95eb273a21fd7522a94607c5f
EdwinZarateP <emzp1994@gmail.com>
2024-01-15 08:36:19 -0500

    modif8

--- src/componentes/Filtros.tsx
+++ src/componentes/Filtros.tsx
@@ -1,4 +1,4 @@
-import '../estilos/Filtros.css'
+import '../estilos/Filtros.css';
 import { FaHouseFloodWater, FaUmbrellaBeach, FaTemperatureArrowUp, FaTemperatureArrowDown } from "react-icons/fa6";
 import { BsTreeFill } from "react-icons/bs";
 import { PiMountainsBold } from "react-icons/pi";
@@ -6,39 +6,31 @@ import { GiDesert } from "react-icons/gi";
 import { MdCabin } from "react-icons/md";
 import { TbFilterEdit } from "react-icons/tb";
 
-
 const Filtros = () => {
-    
-    //creamos una constante con las caracteristicas y sus logos
-
-    const iconos=[
-        {titulo:"Sol", icono:<FaUmbrellaBeach />},
-        {titulo:"Clima calido", icono:<FaTemperatureArrowUp />},
-        {titulo:"Clima frio", icono:<FaTemperatureArrowDown />},
-        {titulo:"Naturaleza", icono:<BsTreeFill />},
-        {titulo:"En la montaña", icono:<PiMountainsBold />},
-        {titulo:"Desierto", icono:<GiDesert />},
-        {titulo:"Flotantes", icono:<FaHouseFloodWater />},
-        {titulo:"Cabañas", icono:<MdCabin />}
-       
+    const iconos = [
+        {titulo: "Sol", icono: <FaUmbrellaBeach />},
+        {titulo: "Clima calido", icono: <FaTemperatureArrowUp />},
+        {titulo: "Clima frio", icono: <FaTemperatureArrowDown />},
+        {titulo: "Naturaleza", icono: <BsTreeFill />},
+        {titulo: "En la montaña", icono: <PiMountainsBold />},
+        {titulo: "Desierto", icono: <GiDesert />},
+        {titulo: "Flotantes", icono: <FaHouseFloodWater />},
+        {titulo: "Cabañas", icono: <MdCabin />}
     ];
-    
-    
+
     return (
         <div className='contenedorFiltros'>
-
-            {/* Utilizamos map para recorrer el array de iconos y renderizar cada elemento */}
-            {iconos.map((elemento, index) => (
-            <div key={index} className="filtro">
-                <span>{elemento.titulo}</span>
-                <div className="icono">{elemento.icono}</div>
+            <div className='filtros-wrapper'>
+                {iconos.map((elemento, index) => (
+                    <div key={index} className="filtro">
+                        <span>{elemento.titulo}</span>
+                        <div className="icono">{elemento.icono}</div>
+                    </div>
+                ))}
             </div>
-            ))}
-
             <div className='multifiltro'>
                 <TbFilterEdit />
             </div>
-
         </div>
     );
 };
